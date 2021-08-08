41f0aa27dfae37f98287926ed481818952387bbf
codezerodawn <codezerodawn@gmail.com>
2021-08-08 17:28:13 +0300

    make decoreator for routes

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from wsgiref.simple_server import make_server
-from own_framework.main import Framework
-from urls import routes
+from my_own_framework.main import Framework
+from views import routes
 
-# create object of WSGI-application
+# Создаем объект WSGI-приложения
 application = Framework(routes)
 
 with make_server('', 8080, application) as httpd:
-    print('Run on the port 8080...')
+    print("Запуск на порту 8080...")
     httpd.serve_forever()
-
--- urls.py
+++
@@ -1,8 +0,0 @@
-from views import Index, About, Contacts
-
-# set of bindings: path - controller
-routes = {
-    '/': Index(),
-    '/about/': About(),
-    '/contacts/': Contacts(),
-}
